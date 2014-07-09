3202374e6576c04b8b57e40b0d31e5a32f3e2829
Bin Zhang <30135878@qq.com>
2014-07-09 17:51:01 +0800

    Fix加错pod后编译不成功

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '5.0'
+# ignore all warnings from all pods
+inhibit_all_warnings!
+pod 'ZBUtilities', '~> 0.0.3'
+pod 'MBProgressHUD', '~> 0.8'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 8
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
+
+// ZBUtilities
+#define COCOAPODS_POD_AVAILABLE_ZBUtilities
+#define COCOAPODS_VERSION_MAJOR_ZBUtilities 0
+#define COCOAPODS_VERSION_MINOR_ZBUtilities 0
+#define COCOAPODS_VERSION_PATCH_ZBUtilities 4
+
