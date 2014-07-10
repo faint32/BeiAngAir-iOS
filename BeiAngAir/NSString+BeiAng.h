4675bc6b6f2de41c1aafc151440411d34cf9f35e
Bin Zhang <30135878@qq.com>
2014-07-10 17:30:26 +0800

    Add NSString+BeiAng category

---
+++ BeiAngAir/NSString+BeiAng.h
@@ -0,0 +1,15 @@
+//
+//  NSString+BeiAng.h
+//  BeiAngAir
+//
+//  Created by zhangbin on 7/10/14.
+//  Copyright (c) 2014 BroadLink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (BeiAng)
+
++ (instancetype)citiesCodeString;
+
+@end
