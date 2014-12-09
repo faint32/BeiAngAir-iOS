65aa5b19622e0f6bc3e8e7da4ff9cc9adc9c96e4
Bin Zhang <30135878@qq.com>
2014-12-09 15:08:39 +0800

    注销后返回上一级界面。

--- BeiAngAir/BLHelpTableViewController.m
+++ BeiAngAir/BLHelpTableViewController.m
@@ -57,7 +57,7 @@ const NSString *sectionSelector = @"sectionSelector";
 		[self hideHUD:YES];
 		if (!error) {
 			[self displayHUDTitle:nil message:@"注销成功" duration:1];
-			[self.tableView reloadData];
+			[self performSelector:@selector(dismiss) withObject:nil afterDelay:1];
 		} else {
 			[self displayHUDTitle:nil message:error.userInfo[BL_ERROR_MESSAGE_IDENTIFIER]];
 		}
